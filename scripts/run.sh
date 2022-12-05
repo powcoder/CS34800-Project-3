bff21f51d153e35704eb6b222594c4129d62b480
powcoder <powcoder@163.com>
2022-12-05 13:26:17 +0800

    CS34800 Project 3

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+echo exit | sqlplus puid@csora/password @drop.sql >/dev/null
+echo exit | sqlplus puid@csora/password @create.sql >/dev/null
+echo exit | sqlplus puid@csora/password @init.sql >/dev/null
+
+javac -cp .:ojdbc8.jar Project3.java
+java -cp .:ojdbc8.jar Project3 input.txt output.txt
